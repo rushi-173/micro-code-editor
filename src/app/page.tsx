af91de3ca08a11193593073218a80dd0cdbf1dd3
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2024-11-16 16:01:14 +0530

    feat: created basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Code Editor</h1>
+    <div className="w-full h-dvh flex flex-col divide-y-2 divide-slate-950/10">
+      <div className="w-full h-12 px-4 py-2 flex items-center">
+        <p>JS Code</p>
+      </div>
+      <div className="w-full h-12"></div>
+      <div className="w-full flex-1">
+        <div className="w-full h-full flex flex-row items-center divide-x-2 divide-slate-950/10">
+          <div className="w-full h-full max-w-md overflow-y-auto"></div>
+          <div className="w-full h-full overflow-y-auto"></div>
+        </div>
+      </div>
+      <div className="w-full h-12"></div>
     </div>
   );
 }
